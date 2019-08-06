04870feeab713f7ecb166d69d74d9e74f36ff335
yjw <youjunwei@fdc.com.cn>
2019-08-06 14:02:15 +0800

    add

--- src/main/java/com/fdc/active/dao/WxFormIdMapper.java
+++
@@ -1,9 +0,0 @@
-package com.fdc.active.dao;
-
-import com.fdc.active.domain.WxFormId;
-
-public interface WxFormIdMapper {
-    int insert(WxFormId record);
-
-    int insertSelective(WxFormId record);
-}
